b8a48bd3536e33c8ae1afe590788fe1d7ffe1a18
s0nnyday <lijnar100@naver.com>
2024-01-14 23:41:14 +0900

    회원 웹 기능 - 등록

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,11 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -14,4 +17,21 @@ public class MemberController {
         this.memberService = memberService;
     }
     //DI 주입의 3가지 방법 : 생성자주입>Setter주입>필드 주입
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        System.out.println("member = " + member.getName());
+        
+        memberService.join(member);
+
+        return "redirect:/";
+    }
 }
