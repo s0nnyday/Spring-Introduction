7794bcf277bcbefe14ce59973bde065d352b2eb1
s0nnyday <lijnar100@naver.com>
2024-01-22 10:11:22 +0900

    AOP 적용

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -18,6 +18,7 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+        System.out.println("memberService = " + memberService.getClass());
     }
     //DI 주입의 3가지 방법 : 생성자주입>Setter주입>필드 주입
 
